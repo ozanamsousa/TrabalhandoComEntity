8eb8a30c1ad3388203ec7b4c4fb3a578a06d7f95
ozanamsousa <ozanamsousa@gmail.com>
2022-08-20 14:52:19 -0300

    evento delete

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -46,5 +46,16 @@ namespace TrabalhandoComEntity.Controllers
             _context.SaveChanges();
             return Ok(contatoBanco);
         }
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+            if(contatoBanco == null)
+            return NotFound();
+
+            _context.Contatos.Remove(contatoBanco);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
 }
